1e2053d4533a455fdb15ddab7039c32f925d2114
annmariya001 <36834099+annmariya001@users.noreply.github.com>
2024-07-16 17:00:47 +0530

    Add files via upload

---
+++ simple-contact-form.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:simple contact form
+ * Description:simple contact form tutorial
+ * Author:Ann Mariya
+ * Version :1.0.0
+ * Text Domain:simple-contact-form
+ * 
+ */
+if (!defined('ABSPATH')) {
+    echo 'What are you trying to do?';
+    exit;
+}
